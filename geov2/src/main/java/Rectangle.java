e8e3179d5da06b1723ace59c4fc26d86d76f1afc
SedaYurdakul <Seda.ztbk@gmail.com>
2021-06-20 18:54:26 +0100

    Rectangle class width,length added

--- geov2/src/main/java/Rectangle.java
+++ geov2/src/main/java/Rectangle.java
@@ -1,2 +1,14 @@
 public class Rectangle {
+    double width = 1.0;
+    double length = 1.0;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+
 }
