6fd00e7b6d16d28d3c386c1782e25847b0308340
emanuelasavi <asaviemanuel@gmail.com>
2021-06-23 09:59:18 +0100

    Square task done

---
+++ geov2/src/main/java/Square.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Square {
+
+    public int length;
+
+    public double squareArea() {
+        return length * length;
+    }
+
+    public double squarePerimeter(){
+        return 4 * length;
+    }
+}
